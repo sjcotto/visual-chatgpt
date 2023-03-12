fa2fe71f7d9145bdb0fb6904bdcd7f7b468d3b9e
Santiago Cotto <sjcotto@gmail.com>
2023-03-12 13:26:20 -0300

    Update visual_chatgpt.py

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -801,27 +801,27 @@ class ConversationBot:
     def __init__(self):
         print("Initializing VisualChatGPT")
         self.llm = OpenAI(temperature=0)
-        self.edit = ImageEditing(device="cuda:6")
-        self.i2t = ImageCaptioning(device="cuda:4")
-        self.t2i = T2I(device="cuda:1")
+        self.edit = ImageEditing(device="cuda:0")
+        self.i2t = ImageCaptioning(device="cuda:0")
+        self.t2i = T2I(device="cuda:0")
         self.image2canny = image2canny()
-        self.canny2image = canny2image(device="cuda:1")
+        self.canny2image = canny2image(device="cuda:0")
         self.image2line = image2line()
-        self.line2image = line2image(device="cuda:1")
+        self.line2image = line2image(device="cuda:0")
         self.image2hed = image2hed()
-        self.hed2image = hed2image(device="cuda:2")
+        self.hed2image = hed2image(device="cuda:0")
         self.image2scribble = image2scribble()
-        self.scribble2image = scribble2image(device="cuda:3")
+        self.scribble2image = scribble2image(device="cuda:0")
         self.image2pose = image2pose()
-        self.pose2image = pose2image(device="cuda:3")
-        self.BLIPVQA = BLIPVQA(device="cuda:4")
+        self.pose2image = pose2image(device="cuda:0")
+        self.BLIPVQA = BLIPVQA(device="cuda:0")
         self.image2seg = image2seg()
-        self.seg2image = seg2image(device="cuda:7")
+        self.seg2image = seg2image(device="cuda:0")
         self.image2depth = image2depth()
-        self.depth2image = depth2image(device="cuda:7")
+        self.depth2image = depth2image(device="cuda:0")
         self.image2normal = image2normal()
-        self.normal2image = normal2image(device="cuda:5")
-        self.pix2pix = Pix2Pix(device="cuda:3")
+        self.normal2image = normal2image(device="cuda:0")
+        self.pix2pix = Pix2Pix(device="cuda:0")
         self.memory = ConversationBufferMemory(memory_key="chat_history", output_key='output')
         self.tools = [
             Tool(name="Get Photo Description", func=self.i2t.inference,
@@ -840,9 +840,9 @@ class ConversationBot:
             Tool(name="Instruct Image Using Text", func=self.pix2pix.inference,
                  description="useful when you want to the style of the image to be like the text. like: make it look like a painting. or make it like a robot. "
                              "The input to this tool should be a comma seperated string of two, representing the image_path and the text. "),
-            Tool(name="Answer Question About The Image", func=self.BLIPVQA.get_answer_from_question_and_image,
-                 description="useful when you need an answer for a question based on an image. like: what is the background color of the last image, how many cats in this figure, what is in this figure. "
-                             "The input to this tool should be a comma seperated string of two, representing the image_path and the question"),
+#             Tool(name="Answer Question About The Image", func=self.BLIPVQA.get_answer_from_question_and_image,
+#                  description="useful when you need an answer for a question based on an image. like: what is the background color of the last image, how many cats in this figure, what is in this figure. "
+#                              "The input to this tool should be a comma seperated string of two, representing the image_path and the question"),
             Tool(name="Edge Detection On Image", func=self.image2canny.inference,
                  description="useful when you want to detect the edge of the image. like: detect the edges of this image, or canny detection on image, or peform edge detection on this image, or detect the canny image of this image. "
                              "The input to this tool should be a string, representing the image_path"),
@@ -867,21 +867,21 @@ class ConversationBot:
             Tool(name="Generate Image Condition On Segmentations", func=self.seg2image.inference,
                  description="useful when you want to generate a new real image from both the user desciption and segmentations. like: generate a real image of a object or something from this segmentation image, or generate a new real image of a object or something from these segmentations. "
                              "The input to this tool should be a comma seperated string of two, representing the image_path and the user description"),
-            Tool(name="Predict Depth On Image", func=self.image2depth.inference,
-                 description="useful when you want to detect depth of the image. like: generate the depth from this image, or detect the depth map on this image, or predict the depth for this image. "
-                             "The input to this tool should be a string, representing the image_path"),
+#             Tool(name="Predict Depth On Image", func=self.image2depth.inference,
+#                  description="useful when you want to detect depth of the image. like: generate the depth from this image, or detect the depth map on this image, or predict the depth for this image. "
+#                              "The input to this tool should be a string, representing the image_path"),
             Tool(name="Generate Image Condition On Depth",  func=self.depth2image.inference,
                  description="useful when you want to generate a new real image from both the user desciption and depth image. like: generate a real image of a object or something from this depth image, or generate a new real image of a object or something from the depth map. "
                              "The input to this tool should be a comma seperated string of two, representing the image_path and the user description"),
-            Tool(name="Predict Normal Map On Image", func=self.image2normal.inference,
-                 description="useful when you want to detect norm map of the image. like: generate normal map from this image, or predict normal map of this image. "
-                             "The input to this tool should be a string, representing the image_path"),
+#             Tool(name="Predict Normal Map On Image", func=self.image2normal.inference,
+#                  description="useful when you want to detect norm map of the image. like: generate normal map from this image, or predict normal map of this image. "
+#                              "The input to this tool should be a string, representing the image_path"),
             Tool(name="Generate Image Condition On Normal Map", func=self.normal2image.inference,
                  description="useful when you want to generate a new real image from both the user desciption and normal map. like: generate a real image of a object or something from this normal map, or generate a new real image of a object or something from the normal map. "
                              "The input to this tool should be a comma seperated string of two, representing the image_path and the user description"),
-            Tool(name="Sketch Detection On Image", func=self.image2scribble.inference,
-                 description="useful when you want to generate a scribble of the image. like: generate a scribble of this image, or generate a sketch from this image, detect the sketch from this image. "
-                             "The input to this tool should be a string, representing the image_path"),
+#             Tool(name="Sketch Detection On Image", func=self.image2scribble.inference,
+#                  description="useful when you want to generate a scribble of the image. like: generate a scribble of this image, or generate a sketch from this image, detect the sketch from this image. "
+#                              "The input to this tool should be a string, representing the image_path"),
             Tool(name="Generate Image Condition On Sketch Image", func=self.scribble2image.inference,
                  description="useful when you want to generate a new real image from both the user desciption and a scribble image or a sketch image. "
                              "The input to this tool should be a comma seperated string of two, representing the image_path and the user description"),
